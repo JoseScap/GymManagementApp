9a9855e3f230f1146106cb7e0abd1779ed2a8ae0
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-07-27 14:45:46 -0300

    Add AppBreadcrumbs.tsx

---
+++ src/modules/core/components/AppBreadcrumbs.tsx
@@ -0,0 +1,21 @@
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import {Breadcrumbs} from "@mui/joy";
+
+interface Props {
+  items: React.ReactNode[];
+}
+
+const AppBreadcrumbs: React.FC<Props> = ({ items }) => {
+  return <Breadcrumbs
+    size="sm"
+    aria-label="breadcrumbs"
+    separator={<ChevronRightRoundedIcon fontSize="sm" />}
+    sx={{ pl: 0 }}
+  >
+    {items.map((item, index) => (
+      <span key={index}>{item}</span>
+    ))}
+  </Breadcrumbs>
+}
+
+export default AppBreadcrumbs;
