83565a8ecea06725e5ca1edf4f6cce34576cd50b
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-07-26 21:12:21 -0300

    Fix building errors (#3)

--- src/modules/common/layouts/PageLayout.tsx
+++ src/modules/common/layouts/PageLayout.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren} from "react";
+import {PropsWithChildren} from "react";
 import {
   Box, CssVarsProvider
 } from "@mui/joy";
--- src/modules/core/pages/HomePage.tsx
+++ src/modules/core/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {Typography} from "@mui/joy";
 
 const HomePage: React.FC = () => {
