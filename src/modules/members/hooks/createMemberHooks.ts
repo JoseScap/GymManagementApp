a5ecdbb4db8390b58785f85068401b3025729f2a
Francisco Perez <franutn23@gmail.com>
2024-07-29 22:11:14 -0300

    Add the context to the component

--- src/modules/members/hooks/createMemberHooks.ts
+++ src/modules/members/hooks/createMemberHooks.ts
@@ -1,43 +1,44 @@
-// import { MemberListContext } from "../contexts/MemberListContext.tsx";
-// import { SetStateAction, useContext } from "react";
-// import { Member } from "../../common/types/members";
-// import axios, { AxiosResponse } from "axios";
-// import { PaginatedApiResponse } from "../../common/types/api";
-
-import { useContext } from "react";
+import { SetStateAction, useContext } from "react";
 import { MemberCreateContext } from "../contexts/CreateMemberContext";
+// import axios, { AxiosResponse } from "axios";
 
 // Interface that should be return by the hook
 interface CreateMemberHooks {
-  changeFullName: (fullName: string) => void;
-  changeDni: (dni: string) => void;
-  changePhoneNumber: (phoneNumber: string) => void;
-  handleCreateMember: () => void /* Está será una petición de AXIOS */;
+  changeFullName: (fullName: SetStateAction<string>) => void;
+  changeDni: (dni: SetStateAction<string>) => void;
+  changePhoneNumber: (phoneNumber: SetStateAction<string>) => void;
+  handleCreateMember: () => Promise<void> /* Está será una petición de AXIOS */;
 }
 
 export const useCreateMember = (): CreateMemberHooks => {
   const { fullName, dni, phoneNumber, setFullName, setDni, setPhoneNumber } =
     useContext(MemberCreateContext);
 
-  const handleCreateMember = () => {
-    console.log("Test: ", fullName, dni, phoneNumber);
-    // await axios.post('http://localhost:3000/members', {
-    // fullName: fullName,
-    // dni: dni,
-    // phoneNumber: phoneNumber
-    // })
+  const changeFullName = (fullName: SetStateAction<string>) => {
+    setFullName(fullName);
   };
 
-  const changeFullName = (fullNameByUser: string) => {
-    setFullName(fullNameByUser);
-  };
-
-  const changeDni = (dniByUser: string) => {
+  const changeDni = (dniByUser: SetStateAction<string>) => {
     setDni(dniByUser);
   };
 
-  const changePhoneNumber = (phoneNumberByUser: string) => {
-    setPhoneNumber(phoneNumberByUser);
+  const changePhoneNumber = (phoneNumber: SetStateAction<string>) => {
+    setPhoneNumber(phoneNumber);
+  };
+
+  const handleCreateMember = async () => {
+    console.log("Test: ", fullName, dni, phoneNumber);
+    if(!!fullName && !!dni) {
+      console.log("No se puede crear el socio");
+      return;
+    }
+
+        // await axios.post("http://localhost:3001/members", {
+        //     fullName,
+        //     dni,
+        //     phoneNumber,
+        // });
+
   };
 
   return {
--- src/modules/members/routers/MemberRouter.tsx
+++ src/modules/members/routers/MemberRouter.tsx
@@ -1,12 +1,17 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import CreateMemberPage from "../pages/CreateMemberPage.tsx";
 import MemberListPage from "../pages/MemberListPage.tsx";
-import {MemberListProvider} from "../contexts/MemberListContext.tsx";
+import { MemberListProvider } from "../contexts/MemberListContext.tsx";
+import { CreateMemberProvider } from "../contexts/CreateMemberContext.tsx";
 
 const MemberRouter: React.FC = () => {
   return (
     <Routes>
-      <Route path="/create" element={<CreateMemberPage />} />
+      <Route path="/create" element={
+        <CreateMemberProvider>
+          <CreateMemberPage />
+        </CreateMemberProvider>
+      } />
       <Route
         path="/list"
         element={
