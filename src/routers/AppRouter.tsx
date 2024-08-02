0a8f850bd2a0ad40234d61d9aeedd3269b3af1aa
Francisco Perez <franutn23@gmail.com>
2024-08-02 18:46:51 -0300

    Add the suscription to the aside component

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -2,12 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import CoreRouter from "../modules/core/routers/CoreRouter.tsx";
 import MemberRouter from "../modules/members/routers/MemberRouter.tsx";
 import PageLayout from "../modules/common/layouts/PageLayout.tsx";
+import SubscriptionRouter from "../modules/subscriptions/routers/SubscriptionRouter.tsx";
 
 const AppRouter: React.FC = () => {
   return <PageLayout>
     <Routes>
       <Route path="/*" element={<CoreRouter />} />
       <Route path="/members/*" element={<MemberRouter />} />
+      <Route path="/subscriptions/*" element={<SubscriptionRouter />} />
     </Routes>
   </PageLayout>
 }
